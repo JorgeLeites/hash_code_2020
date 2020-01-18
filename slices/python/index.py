9d1214ab45a672966eef696676267f3e55d606e0
Jorge Leites <jorge.l4998@gmail.com>
2020-01-18 00:41:14 -0300

    Changed the algorithm

--- slices/python/index.py
+++ slices/python/index.py
@@ -11,44 +11,46 @@ pizzas = [int(pizza_slices) for pizza_slices in file.readline().split(' ')]
 file.close()
 
 # Do the math
-max_values = [{'slices': 0, 'pizzas': [False for pizza in pizzas]}]
+max_values = [[0] for pizza in pizzas]
+max_values.append([0])
+for index in range(1, max_slices + 1):
+    max_values[0].append(0)
 
 for index in range(1, max_slices + 1):
     print(str(100 * index / max_slices) + '%')
-    candidate_values = max_values[index-1].copy()
     for pizza_number, pizza_slices in enumerate(pizzas):
+        pizza_number += 1
         if pizza_slices > index:
-            break  # The list of pizzas is in non descendent order,
-                   # so if we find one that doesn't fit, we can stop checking
-
-        # We have to find a set of selected pizzas that doesn't contain the current one
-        valid_index = index - pizza_slices
-        found_index = False
-        while not found_index and index >= 0:
-            if max_values[valid_index]['pizzas'][pizza_number]:
-                valid_index -= 1
-            else:
-                found_index = True
-
-        if found_index:
-            current_slices = max_values[valid_index]['slices'] + pizza_slices
-            if current_slices > candidate_values['slices']:
-                candidate_values['slices'] = current_slices
-                candidate_values['pizzas'] = max_values[valid_index]['pizzas'][:]
-                candidate_values['pizzas'][pizza_number] = True
-    max_values.append(candidate_values)
+            max_values[pizza_number].append(max_values[pizza_number - 1][index])
+        else:
+            value_with = max_values[pizza_number - 1][index - pizza_slices] + pizza_slices
+            value_without = max_values[pizza_number - 1][index]
+            max_value = max(value_with, value_without)
+            max_values[pizza_number].append(max_value)
 
 # Final results
-total_pizzas = 0
 selected_pizzas = []
-for index, selected_pizza in enumerate(max_values[max_slices]['pizzas']):
-    if selected_pizza:
-        selected_pizzas.append(str(index))
-        total_pizzas += 1
+def find_solution(pizza_index, amount_index):
+    if pizza_index > 0 and amount_index > 0:
+        pizza_slices = pizzas[pizza_index - 1]
+        if pizza_slices <= amount_index:
+            value_with = max_values[pizza_index - 1][amount_index - pizza_slices] + pizza_slices
+            value_without = max_values[pizza_index - 1][amount_index]
+            if value_with > value_without:
+                selected_pizzas.append(pizza_index - 1)
+                find_solution(pizza_index - 1, amount_index - pizza_slices)
+            else:
+                find_solution(pizza_index - 1, amount_index)
+        else:
+            find_solution(pizza_index - 1, amount_index)
+
+find_solution(amount_of_pizzas, max_slices)
+selected_pizzas.sort()
+selected_pizzas = [str(selected_pizza) for selected_pizza in selected_pizzas]
 
 # Write the output file
 output_file = input_file[slice(-2)] + 'out'
 file = open(output_file, 'w+')
-file.write(str(total_pizzas) + '\n')
+file.write(str(len(selected_pizzas)) + '\n')
 file.write(' '.join(selected_pizzas))
 file.close()
